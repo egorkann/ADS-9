c6b06f70459f1a5349a1ea3fc7d9d6e7db658087
egorkann <minicraftnn@mail.ru>
2025-06-06 18:32:12 +0300

    Update tree.cpp

--- src/tree.cpp
+++ src/tree.cpp
@@ -1,1 +1,14 @@
+// Copyright 2025 NNTU-CS
+#include "tree.h"
 
+PMTree::PMTree(const std::vector<char>& elems) : elements_(elems) {
+  root_ = std::make_shared<Node>('\0');
+}
+
+std::shared_ptr<Node> PMTree::getRoot() const {
+  return root_;
+}
+
+std::vector<char> PMTree::getElements() const {
+  return elements_;
+}
