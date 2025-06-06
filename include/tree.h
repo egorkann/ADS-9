46140919014237025677c10e3e0658d0d88e3600
egorkann <minicraftnn@mail.ru>
2025-06-06 18:03:11 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,28 +1,27 @@
 // Copyright 2022 NNTU-CS
-#ifndef INCLUDE_TREE_H_
-#define INCLUDE_TREE_H_
+#ifndef TREE_H_
+#define TREE_H_
+
 #include <vector>
 #include <memory>
 
 class PMNode {
-public:
-    char value;
-    std::vector<std::shared_ptr<PMNode>> children;
+ public:
+  int value;
+  std::vector<std::shared_ptr<PMNode>> children;
 
-    PMNode(char val) : value(val) {}
+  explicit PMNode(int val);
 };
 
 class PMTree {
-public:
-    std::shared_ptr<PMNode> root;
-    std::vector<std::vector<char>> allPerms;
-    std::vector<char> elements;
+ public:
+  std::shared_ptr<PMNode> root;
 
-    PMTree(const std::vector<char>& elems);
+  explicit PMTree(int root_value);
+  void addNode(const std::shared_ptr<PMNode>& parent, int child_value);
+  void getAllPerms(const std::shared_ptr<PMNode>& node,
+                   std::vector<std::vector<int>>& perms,
+                   std::vector<int>& current);
 };
 
-std::vector<std::vector<char>> getAllPerms(PMTree& tree);
-std::vector<char> getPerm1(PMTree& tree, int num);
-std::vector<char> getPerm2(PMTree& tree, int num);
-
-#endif  // INCLUDE_TREE_H_
+#endif  // TREE_H_
