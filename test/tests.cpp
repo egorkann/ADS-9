9b06d47234ddd11183819e9109bc7f78fd79a5fb
egorkann <minicraftnn@mail.ru>
2025-06-06 18:31:12 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,28 +1,30 @@
 // Copyright 2025 NNTU-CS
+#include "alg.h"
+#include "tree.h"
 #include <gtest/gtest.h>
 #include <vector>
-#include "alg.h"
 
 TEST(ads9, test1) {
-    PMTree tree({'a', 'b'});
-    std::vector<char> result = getPerm1(tree, 1);
-    ASSERT_EQ(result, std::vector<char>({'a', 'b'}));
+  PMTree tree({'a', 'b', 'c'});
+  std::vector<char> result = getPerm1(tree, 1);
+  EXPECT_EQ(result, std::vector<char>({'a', 'b', 'c'}));
 }
 
 TEST(ads9, test2) {
-    PMTree tree({'a', 'b'});
-    std::vector<char> result = getPerm2(tree, 2);
-    ASSERT_EQ(result, std::vector<char>({'b', 'a'}));
+  PMTree tree({'a', 'b', 'c'});
+  std::vector<char> result = getPerm2(tree, 2);
+  EXPECT_EQ(result, std::vector<char>({'a', 'c', 'b'}));
 }
 
 TEST(ads9, test3) {
-    PMTree tree({'a', 'b', 'c'});
-    std::vector<char> result = getPerm1(tree, 6);
-    ASSERT_EQ(result, std::vector<char>({'c', 'b', 'a'}));
+  PMTree tree({'x', 'y', 'z'});
+  std::vector<char> result = getPerm1(tree, 6);
+  EXPECT_EQ(result, std::vector<char>({'z', 'y', 'x'}));
 }
 
 TEST(ads9, test4) {
-    PMTree tree({'a', 'b', 'c'});
-    std::vector<char> result = getPerm2(tree, 4);
-    ASSERT_EQ(result, std::vector<char>({'b', 'c', 'a'}));
+  PMTree tree({'1', '2', '3'});
+  std::vector<char> result = getPerm2(tree, 5);
+  EXPECT_EQ(result, std::vector<char>({'3', '1', '2'}));
 }
+
