4f1bf6baa37936b7a38fd6ba2d159737030ee241
egorkann <minicraftnn@mail.ru>
2025-06-06 18:13:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,18 +8,18 @@ PMTree::PMTree(const std::vector<char>& elems) : elements_(elems) {
 }
 
 std::vector<std::vector<char>> getAllPerms(PMTree& tree) {
-    if (tree.getAllPerms().empty()) {
-        buildTree(tree.root, tree.getElements());
-        std::vector<char> current;
-        collectPerms(tree.root, current, tree.getAllPerms());
-    }
-    return tree.getAllPerms();
+  if (tree.getAllPerms().empty()) {
+    buildTree(tree.root, tree.getElements());
+    std::vector<char> current;
+    collectPerms(tree.root, current, tree.getAllPerms());
+  }
+  return tree.getAllPerms();
 }
 
 std::vector<char> getPerm2(PMTree& tree, int index) {
-    std::vector<char> elems = tree.getElements();
-    if (index >= 0 && index < static_cast<int>(elems.size())) {
-        return {elems[index]};
-    }
-    return {};
+  std::vector<char> elems = tree.getElements();
+  if (index >= 0 && index < static_cast<int>(elems.size())) {
+    return {elems[index]};
+  }
+  return {};
 }
