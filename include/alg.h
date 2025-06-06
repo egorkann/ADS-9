2e0b9873f9b8b20e3f8409d08af8c93c90aba17f
egorkann <minicraftnn@mail.ru>
2025-06-06 18:17:23 +0300

    Update alg.h

--- include/alg.h
+++ include/alg.h
@@ -7,6 +7,7 @@
 #include "tree.h"
 
 std::vector<std::vector<char>> getAllPerms(PMTree& tree);
+std::vector<char> getPerm1(PMTree& tree, int index);
 std::vector<char> getPerm2(PMTree& tree, int index);
 
 void buildTree(std::shared_ptr<Node> root, const std::vector<char>& elements);
