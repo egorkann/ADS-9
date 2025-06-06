f19ee566652682c8d1d191cf94e7e66a269e064f
egorkann <minicraftnn@mail.ru>
2025-06-06 18:43:44 +0300

    Update alg.h

--- include/alg.h
+++ include/alg.h
@@ -3,10 +3,13 @@
 #define INCLUDE_ALG_H_
 
 #include "tree.h"
+
 #include <memory>
 #include <vector>
 
-void buildTree(std::shared_ptr<PMNode> node, const std::vector<char>& remaining);
+void buildTree(std::shared_ptr<PMNode> node,
+               const std::vector<char>& remaining);
+
 void collectPerms(std::shared_ptr<PMNode> node, std::vector<char>& current,
                   std::vector<std::vector<char>>& result);
 
