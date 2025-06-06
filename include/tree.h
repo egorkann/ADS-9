f20ffa24bdf5b606bb4e22482fbdbb9ad38b0643
egorkann <minicraftnn@mail.ru>
2025-06-06 18:20:22 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -6,25 +6,22 @@
 #include <memory>
 
 struct Node {
-  char value;
+  char val;
   std::vector<std::shared_ptr<Node>> children;
-  explicit Node(char v) : value(v) {}
+  explicit Node(char v) : val(v) {}
 };
 
 class PMTree {
  public:
-  explicit PMTree(int root_value);
-  explicit PMTree(const std::vector<char>& elems);
-
-  const std::vector<char>& getElements() const { return elements_; }
-  const std::vector<std::vector<char>>& getAllPerms() const { return all_perms_; }
-  std::vector<std::vector<char>>& getAllPerms() { return all_perms_; }
+  explicit PMTree(const std::vector<char>& elements);
+  std::vector<char> getElements() const;
+  std::vector<std::vector<char>>& getAllPerms();
 
   std::shared_ptr<Node> root;
 
  private:
   std::vector<char> elements_;
-  std::vector<std::vector<char>> all_perms_;
+  std::vector<std::vector<char>> allPerms_;
 };
 
 #endif  // INCLUDE_TREE_H_
