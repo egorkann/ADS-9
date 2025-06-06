8185491e4a83bc79665bb0386c4d27a8debaee33
egorkann <minicraftnn@mail.ru>
2025-06-06 18:13:19 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,21 +1,21 @@
 // Copyright 2025 NNTU-CS
-#ifndef TREE_H
-#define TREE_H
+#ifndef INCLUDE_TREE_H_
+#define INCLUDE_TREE_H_
 
 #include <vector>
 
 class PMTree {
-public:
-    explicit PMTree(int root_value);
-    PMTree(const std::vector<char>& elems);
+ public:
+  explicit PMTree(int root_value);
+  explicit PMTree(const std::vector<char>& elems);
 
-    const std::vector<char>& getElements() const { return elements_; }
-    const std::vector<std::vector<char>>& getAllPerms() const { return all_perms_; }
-    std::vector<std::vector<char>>& getAllPerms() { return all_perms_; }
+  const std::vector<char>& getElements() const { return elements_; }
+  const std::vector<std::vector<char>>& getAllPerms() const { return all_perms_; }
+  std::vector<std::vector<char>>& getAllPerms() { return all_perms_; }
 
-private:
-    std::vector<char> elements_;
-    std::vector<std::vector<char>> all_perms_;
+ private:
+  std::vector<char> elements_;
+  std::vector<std::vector<char>> all_perms_;
 };
 
-#endif
+#endif  // INCLUDE_TREE_H_
